df3ec9eb3182d96eb826143737d861159527cbe0
Jasper Swallen <swallen@usc.edu>
2022-09-07 23:08:24 -0700

    working program
    timing is noticeably off

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,13 +1,15 @@
-#include <mbed.h>
+#include <rtos.h>
 
-int main()
+extern "C" int mbed_start();
+
+int mbed_start()
 {
     printf("Hello, world!\r\n");
 
     int count = 0;
     while (1)
     {
-        ThisThread::sleep_for(100ms);
+        ThisThread::sleep_for(std::chrono::milliseconds(1000));
         printf("%d\r\n", count++);
     }
 }
